9b9c7bdcdb54caa3b114e7cf6c45d346854b8d44
Mustafa Eftekin <m_eftekin@hotmail.com>
2024-04-21 01:15:46 +0300

    update pillow.py

--- computer_vision_and_image_processing/digital_image/pillow.py
+++ computer_vision_and_image_processing/digital_image/pillow.py
@@ -1,9 +1,20 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from PIL import Image, ImageOps
+
+
+def get_concat_h(im1, im2):
+    dst = Image.new("RGB", (im1.width + im2.width, im1.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (im1.width, 0))
+    return dst
+
 
 image = Image.open("../../assets/darth_vader.jpg")
 
 plt.imshow(image)
+plt.show()
+plt.clf()
 
 print("Format of the image:", image.format)
 print("Size of the image:", image.size)
@@ -15,5 +26,18 @@ image_gray.show(title="darth_vader_gray")
 print("Mode of the gray image:", image_gray.mode)  # L means luminance
 
 red, green, blue = image.split()
-red.show(title="darth_vader_red")
-green.show(title="darth_vader_green")
+
+get_concat_h(image, red).show()
+get_concat_h(image, green).show()
+get_concat_h(image, blue).show()
+
+
+# get darth vader without blue
+blue_darth_vader = image
+blue_array = np.array(blue_darth_vader)
+blue_array[:, :, 2] = 0
+
+plt.figure(figsize=(10, 10))
+plt.imshow(blue_array)
+plt.show()
+plt.clf()
