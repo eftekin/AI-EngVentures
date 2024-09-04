5195003d9136e00bd6f4296d4377e3834fd766c4
Mustafa Eftekin <75522456+eftekin@users.noreply.github.com>
2024-09-04 18:14:07 +0300

    feat(simple_regression.py): add simple regression script for predicting rent based on square footage and number of burglaries using Linear Regression model

---
+++ machine_learning/supervised_learning/simple_regression.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# Load the data
+housing_data = pd.read_csv("../../assets/csv/housing_data.csv")
+X = housing_data[["Sq ft", "Burglaries"]]
+y = housing_data["Rent"]
+
+# Create the model
+reg = LinearRegression()
+
+# Train the model
+reg.fit(X, y)
+
+square_footage = 950
+number_of_burglaries = 2
+
+y_pred = reg.predict(np.array([square_footage, number_of_burglaries]).reshape(1, 2))
+
+print(y_pred)
